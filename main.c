85a0dffe6b3776e92a5da2c7e490e9b46ff04bbd
Magomed Aliev <steam.30mb1@gmail.com>
2017-02-21 00:55:50 +0300

    some functions moved to separate file

--- main.c
+++ main.c
@@ -1,48 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 #include <syslog.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <time.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <netdb.h>
-
-int is_ip(char * IP) {
-    char * endptr;
-    long int a;
-    if (a = strtol(IP, &endptr, 10)) {
-        if (a >= 0 && a <= 255) {
-            int i;
-            for (i = 0 ; i < 3; i++) {
-                ++endptr;
-                char * tmp = endptr;
-                if (*(endptr) == 48 && *(endptr+1) == 46 && strtol(endptr, &endptr, 10) == 0) continue;
-                endptr = tmp;
-                if (!(a = strtol(endptr, &endptr, 10)) || !(a >= 0 && a <= 255)) {
-                    return 0;
-                }
-            }
-            return 1;
-        } else {
-            return 0;
-        } 
-    } else {
-        return 0;
-    }
-    return 1;
-}
-
-void gettime() {
-    time_t now;
-    time(&now);
-    char buff[256];
-    strcpy(buff, ctime(&now));
-    buff[strlen(buff)-1]='\0';
-    printf("%s ", buff);
-}
+#include "utility.h"
 
 void handle_request(int client_socket, struct sockaddr_in * client_address)
 {
